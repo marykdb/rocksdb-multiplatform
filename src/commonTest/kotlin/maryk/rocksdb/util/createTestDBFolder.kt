46caaefd9c528a9b4d461efa496d89046a3bc575
Jurriaan Mous <jurmous@jurmo.us>
2025-09-21 15:05:11 +0200

    Fix apple OS initial folder creation issue.

--- src/commonTest/kotlin/maryk/rocksdb/util/createTestDBFolder.kt
+++ src/commonTest/kotlin/maryk/rocksdb/util/createTestDBFolder.kt
@@ -1,8 +1,13 @@
 package maryk.rocksdb.util
 
+import maryk.createFolder
 import kotlin.random.Random
 import kotlin.random.nextUInt
 
+private const val TEST_DB_ROOT = "build/test-database"
+
 @Suppress("EXPERIMENTAL_API_USAGE")
-fun createTestDBFolder(name: String?) =
-    ("build/test-database/${name!!}_" + Random.nextUInt())
+fun createTestDBFolder(name: String?): String {
+    createFolder(TEST_DB_ROOT)
+    return "$TEST_DB_ROOT/${name!!}_" + Random.nextUInt()
+}
